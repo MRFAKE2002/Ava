a6fa2c8cfaeb4462b730f537e2d590498169507b
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-30 17:36:33 +0330

    Add our custom colors as theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+//! Libraries
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+//! CSS
 import './index.css'
+
+//! App
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
