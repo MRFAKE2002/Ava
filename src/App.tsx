ceb4fb72b6f1273b545b5418ce54ed44233f988f
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-06-07 18:20:42 +0330

    Add Speech page in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 //! Components
 import Sidebar from "./components/Sidebar";
+import Speech from "./pages/Speech";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50 overflow-hidden">
       <Sidebar />
-    </>
+      
+      {/* محتوای اصلی */}
+      <main className="relative mr-[166px] top-24 h-screen">
+        <Speech />
+      </main>
+    </div>
   );
 }
 
