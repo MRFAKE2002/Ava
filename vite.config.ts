cae6a1f54c2d602f01fa1ec560aca3b47aabf721
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-07-19 23:13:07 +0330

    Add proxy in vite for api CORS error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://harf.roshan-ai.ir",
+        changeOrigin: true,
+        secure: true,
+        headers: {
+          Authorization: "Token a85d08400c622b50b18b61e239b9903645297196",
+        },
+      },
+    },
+  },
+});
