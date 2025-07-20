e656806ef5499b3c67c5ffb1af76d0c5e447237d
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-07-20 10:45:28 +0330

    Add comment for better understanding

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+//! Vite
 import { defineConfig } from "vite";
+
+//! React
 import react from "@vitejs/plugin-react";
+
+//! TailwindCSS
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    //! CORS
     proxy: {
       "/api": {
         target: "https://harf.roshan-ai.ir",
