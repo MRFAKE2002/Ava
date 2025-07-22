cea10e8ac39c8fdebd2dddc79b2c2e9f48358dfe
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-07-22 23:00:56 +0330

    Add redux provider to project main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 //! Libraries
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 //! CSS
-import './index.css'
+import "./index.css";
 
 //! App
-import App from './App.tsx'
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+//! Redux
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
