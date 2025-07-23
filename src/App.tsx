0cec3d5894201d85ea8df36a727c1a986c46db73
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-07-23 08:29:36 +0330

    Add react router into project app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+//! Libraries
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 //! Components
 import Sidebar from "./components/Sidebar";
 import UserDropdown from "./components/UserDropdown";
+
+//! Pages
 import Speech from "./pages/Speech";
+import Archive from "./pages/Archive";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 overflow-hidden">
-      <Sidebar />
+    <Router>
+      <div className="min-h-screen overflow-hidden">
+        <Sidebar />
 
-      {/* محتوای اصلی */}
-      <main className="relative mr-[166px] top-24 h-screen">
-        <UserDropdown />
-        <Speech />
-      </main>
-    </div>
+        {/* محتوای اصلی */}
+        <main className="relative mr-[166px] top-24 h-screen">
+          <UserDropdown />
+
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Speech />} />
+            <Route path="/speech" element={<Speech />} />
+            <Route path="/archive" element={<Archive />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
